5073151d4a48075fc22a26abd0a752a1ea2fdd9f
k-nasa <htilcs1115@gmail.com>
2019-03-15 18:27:36 +0900

    test: Add init command test

---
+++ tests/init.rs
@@ -0,0 +1,50 @@
+#[cfg(test)]
+mod test {
+    use ruspec::ruspec;
+
+    ruspec! {
+        describe "init command" {
+            before {
+                use std::fs;
+                use std::process::Command;
+                use std::path::Path;
+                const COMMAND: &str = "target/debug/tigr";
+                const SANDBOX_PATH: &str = "sandbox";
+
+                Command::new(COMMAND)
+                    .arg("init")
+                    .arg(SANDBOX_PATH)
+                    .output()
+                    .unwrap();
+            }
+
+            after { fs::remove_dir_all(Path::new(SANDBOX_PATH)).unwrap(); }
+
+            it "should create .git dir" {
+                let path = Path::new(SANDBOX_PATH);
+                let gitdir = path.join(".git");
+                assert!(path.exists());
+                assert!(gitdir.exists());
+            }
+
+            it "should create .git container files" {
+                let path = Path::new(SANDBOX_PATH);
+                let gitdir = path.join(".git");
+                assert!(gitdir.exists());
+                assert!(gitdir.join("branches").exists());
+                assert!(gitdir.join("objects").exists());
+                assert!(gitdir.join("refs/tags").exists());
+                assert!(gitdir.join("refs/heads").exists());
+            }
+
+            // 順番が安定しないのでコメントアウト
+            // it "should valid file content" {
+            //     let path = Path::new(SANDBOX_PATH);
+            //     let config_file = path.join(".git/config");
+            //
+            //     let config = fs::read_to_string(config_file).unwrap();
+            //     assert_eq!(config, include_str!("./support/example.config"));
+            // }
+        }
+    }
+}
