09508112f2a787aed34a15e9a97bd7081286c4bb
k-nasa <htilcs1115@gmail.com>
2019-03-15 17:29:27 +0900

    feat: main logic

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
+mod commands;
 mod repository;
 
+use commands::build_app;
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run() {
+        println!("{}", e);
+    }
+}
+
+fn run() -> Result<(), failure::Error> {
+    let matches = build_app().get_matches();
+
+    match matches.subcommand() {
+        ("init", arg) => commands::init::exec(arg)?,
+        _ => build_app().print_help()?,
+    }
+
+    Ok(())
 }
