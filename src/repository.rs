467799494e508c8fbaab31af63080444c95a799a
k-nasa <htilcs1115@gmail.com>
2019-03-15 15:40:58 +0900

    feat: crate git repository dir

--- src/repository.rs
+++ src/repository.rs
@@ -41,4 +41,59 @@ impl Repository {
             conf: conf,
         })
     }
+
+    /// Create a new repository at path
+    pub fn repository_create(path: &str) -> Result<(), Error> {
+        let repository = Self::new(Path::new(path), true)?;
+
+        if repository.worktree.exists() {
+            if !repository.worktree.is_dir() {
+                bail!("{} is not a directory!", repository.worktree.display())
+            }
+        } else {
+            fs::create_dir_all(repository.worktree.clone())?
+        }
+
+        repository.create_dir("branches")?;
+        repository.create_dir("objects")?;
+        repository.create_dir("refs/tags")?;
+        repository.create_dir("refs/heads")?;
+
+        fs::write(
+            repository.gitdir.join("description"),
+            "Unnamed repository; edit this file 'description' to name the repository.\n",
+        )?;
+
+        fs::write(repository.gitdir.join("HEAD"), "ref: refs/heads/master\n")?;
+
+        let conf = Self::default_config();
+        conf.write_to_file(repository.gitdir.join("config"))?;
+
+        Ok(())
+    }
+
+    fn create_dir(&self, path: &str) -> Result<(), Error> {
+        let path = self.gitdir.join(path);
+
+        if path.exists() {
+            if !path.is_dir() {
+                return Ok(());
+            }
+            bail!("Not a directory {}", path.display())
+        }
+
+        fs::create_dir_all(path)?;
+
+        Ok(())
+    }
+
+    fn default_config() -> Ini {
+        let mut conf = Ini::new();
+        conf.with_section(Some("core".to_owned()))
+            .set("repositoryformatversion", "0")
+            .set("filemode", "false")
+            .set("bare", "false");
+
+        conf
+    }
 }
