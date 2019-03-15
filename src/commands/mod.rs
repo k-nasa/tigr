a14ee83f4ec3a4c58016e12a2314e4541861cccb
k-nasa <htilcs1115@gmail.com>
2019-03-15 17:55:38 +0900

    fix: init command argument

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -8,6 +8,9 @@ pub fn build_app() -> App {
     clap::App::new(crate_name!())
         .version(crate_version!())
         .about(crate_description!())
-        .subcommand(SubCommand::with_name("init").about("Initialize a new, empty repository."))
-        .arg(Arg::with_name("path").help("Where to create the repository."))
+        .subcommand(
+            SubCommand::with_name("init")
+                .about("Initialize a new, empty repository.")
+                .arg(Arg::with_name("path").help("Where to create the repository.")),
+        )
 }
