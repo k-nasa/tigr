4906b99766b4c32da63df93de71593d5e288985b
k-nasa <htilcs1115@gmail.com>
2019-03-19 15:57:40 +0900

    feat: Add GitBlob struct

--- src/git_object.rs
+++ src/git_object.rs
@@ -7,3 +7,26 @@ pub trait GitObject {
     fn serialize(&self) -> &str;
     fn deserialize(&mut self, data: &str) -> ();
 }
+
+pub struct GitBlob {
+    repo: Repository,
+    blobdate: String,
+}
+impl GitBlob {
+    pub fn new(repo: Repository, blobdate: &str) -> Self {
+        GitBlob {
+            repo,
+            blobdate: blobdate.into(),
+        }
+    }
+}
+
+impl GitObject for GitBlob {
+    fn serialize(&self) -> &str {
+        &self.blobdate
+    }
+
+    fn deserialize(&mut self, data: &str) {
+        self.blobdate = data.to_string();
+    }
+}
