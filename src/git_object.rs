2e6a8ea0a4d0ffdb2a5ddfe83d3f6cfb2dff015d
k-nasa <htilcs1115@gmail.com>
2019-03-19 15:57:57 +0900

    feat: Add read_object function

--- src/git_object.rs
+++ src/git_object.rs
@@ -30,3 +30,36 @@ impl GitObject for GitBlob {
         self.blobdate = data.to_string();
     }
 }
+
+pub fn read_object(repo: Repository, sha: &str) -> Result<Box<dyn GitObject>, failure::Error> {
+    let path = repo
+        .gitdir()
+        .join("objects")
+        .join(&sha[0..2])
+        .join(&sha[2..]);
+
+    let zlib_string = std::fs::read_to_string(path)?;
+    let mut z = ZlibDecoder::new(zlib_string.as_bytes());
+    let mut raw = String::new();
+    z.read_to_string(&mut raw)?;
+
+    // Read object type
+    let x = raw.find(' ').unwrap();
+    let fmt = &raw[0..x];
+
+    // Read and validate object size
+    let y = raw.find('\x00').unwrap();
+    let size: usize = raw[x..y].parse()?;
+
+    if size != raw.len() - y - 1 {
+        failure::bail!("Malformed object {}: bad length", sha)
+    }
+
+    match fmt {
+        "commit" => unimplemented!(),
+        "tree" => unimplemented!(),
+        "tag" => unimplemented!(),
+        "blob" => Ok(Box::new(GitBlob::new(repo, &raw[y + 1..]))),
+        _ => failure::bail!("Unknown type {} for object {}", fmt, sha),
+    }
+}
